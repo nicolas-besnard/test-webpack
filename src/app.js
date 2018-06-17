d69eed677c04874b0e3a47b96a76110db2a99559
Nicolas Besnard <besnard.nicolas@gmail.com>
2018-06-17 19:00:54 +1000

    02-babel

---
+++ src/app.js
@@ -0,0 +1,4 @@
+const string = "HELLO WORLD";
+
+let [b,,c] = [1, 2, 3, 4, 5];
+console.log(string, b, c);
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,29 @@
-const path = require('path')
+const path = require('path');
 
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  mode: isProduction ? "production" : "development",
+  mode: isProduction ? 'production' : 'development',
   entry: './src/app.js',
 
   output: {
     path: path.resolve('./dist'),
     filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        //use: [{
+        //  loader: 'babel-loader',
+        //  options: {
+        //    presets: ['@babel/preset-env']
+        //  }
+        //}]
+        use: ['babel-loader']
+      }
+    ]
   }
-}
+};
