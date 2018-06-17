069caa4ae2810eccb353e3966a57b90fc6e5b8dd
Nicolas Besnard <besnard.nicolas@gmail.com>
2018-06-17 20:20:23 +1000

    04-css & sass

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const string = 'HELLO WORLD';
+import css from './app.scss'
 
 let [b, , c] = [1, 2, 3, 4, 5];
 console.log(string, b, c);
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,25 @@
 const path = require('path');
-
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production';
+const cssLoaders = [
+  'style-loader',
+  { loader: 'css-loader', options: { importLoaders: 1 } },
+  {
+    loader: 'postcss-loader',
+    options: {
+      plugins: (loader) => [
+        require('autoprefixer')({
+          browsers: ['last 2 versions', 'ie > 8']
+        })
+      ]
+    }
+  }
+];
 
 let config = {
   mode: isProduction ? 'production' : 'development',
-  devtool: isProduction ? "" : "cheap-module-eval-source-map",
+  devtool: isProduction ? '' : 'cheap-module-eval-source-map',
   entry: './src/app.js',
 
   output: {
@@ -27,13 +40,19 @@ let config = {
         //  }
         //}]
         use: ['babel-loader']
+      }, {
+        test: /\.css$/,
+        use: cssLoaders
+      }, {
+        test: /\.scss$/,
+        use: [...cssLoaders, 'sass-loader']
       }
     ]
-  },
+  }
 };
 
 if (isProduction) {
-  config.plugins.push(new UglifyJSPlugin())
+  config.plugins.push(new UglifyJSPlugin());
 }
 
-module.exports = config
+module.exports = config;
